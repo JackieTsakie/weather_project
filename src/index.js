e43887a414dbb6cc47fcae7d5d4bb5e70023e450
Konstantina Boua <85695279+JackieTsakie@users.noreply.github.com>
2023-05-24 13:03:25 +0300

    Updated js & css

--- src/index.js
+++ src/index.js
@@ -34,15 +34,13 @@ function displayWeatherCondition(response) {
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
   );
-  document.querySelector("#precipitation").innerHTML =
-    response.data.precipitation;
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   console.log(response);
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
   document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+    response.data.weather[0].description;
   console.log(response);
 }
 
